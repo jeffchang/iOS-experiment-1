e9e87740ced754fa7d59802dc8f4b978445317be
Jeff Chang <jeffchang.ca@gmail.com>
2013-10-18 15:44:18 +0100

    first commit

--- MobileCity2/JCZViewController.m
+++ MobileCity2/JCZViewController.m
@@ -8,16 +8,44 @@
 
 #import "JCZViewController.h"
 
-@interface JCZViewController ()
+@interface JCZViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UITextView *viewField;
+@property (strong, nonatomic) NSMutableArray *items;
 
 @end
 
 @implementation JCZViewController
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self addItem:self.inputField.text];
+    [self displayItems];
+    [self clearTextField];
+    return YES;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.inputField becomeFirstResponder];
+    self.items = [[NSMutableArray alloc] initWithCapacity:5];
+}
+
+- (void)clearTextField {
+    self.inputField.text = @"";
+}
+
+- (void)addItem:(NSString *)item {
+    [self.items addObject:item];
+}
+
+- (void)displayItems {
+
+    NSMutableString *itemList = [[NSMutableString alloc]initWithCapacity:200];
+    [self.items enumerateObjectsUsingBlock:^(NSString *item, NSUInteger index, BOOL *stop) {
+        [itemList appendFormat:@"(%d) %@ \n", index + 1, item];
+    }];
+    self.viewField.text = itemList;
 }
 
 - (void)didReceiveMemoryWarning
